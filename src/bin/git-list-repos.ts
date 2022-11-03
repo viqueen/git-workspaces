ff8e2748e010bba4dc24b27753391cf8f51b4a2c
Hasnae <hasnae@viqueen.org>
2022-11-03 11:56:00 +1100

    DEVBOX-#4 simplify list-repos command for now

--- src/bin/git-list-repos.ts
+++ src/bin/git-list-repos.ts
@@ -1,48 +1,13 @@
 #! /usr/bin/env node
 
-import { Item } from '../data/types';
-import { prompt } from 'inquirer';
-import * as path from 'path';
-import { spawn } from 'child_process';
-import {
-    Configuration,
-    getConfiguration,
-    withProgram,
-    WithProgram
-} from '../lib';
+import { getConfiguration, withProgram, WithProgram } from '../lib';
 
-const switchRepoQuestion = async (items: Item[]) => {
-    if (items.length === 0) return undefined;
-    return prompt([
-        {
-            name: 'selectedRepo',
-            type: 'list',
-            message: 'select repo to switch to',
-            choices: items.map((item) => path.join(item.workspace, item.slug)),
-            pageSize: 20
-        }
-    ]);
-};
-
-const switchRepoAnswer = ({
-    workspacesRoot
-}: Pick<Configuration, 'workspacesRoot'>) => {
-    return async (answer: { selectedRepo: string } | undefined) => {
-        if (!answer) return;
-        const target = path.resolve(workspacesRoot, answer.selectedRepo);
-        const child = spawn('bash', ['-i'], { cwd: target, stdio: 'inherit' });
-        child.on('exit', () => process.exit());
-        child.on('close', () => process.exit());
-    };
-};
-
-const listRepos: WithProgram = ({ registry, workspacesRoot }, program) => {
+const listRepos: WithProgram = ({ registry }, program) => {
     program.action(async (opts) => {
         const { workspace } = opts;
         await registry
             .list((item) => item.workspace === workspace)
-            .then(switchRepoQuestion)
-            .then(switchRepoAnswer({ workspacesRoot }));
+            .then(console.table);
     });
 };
 
