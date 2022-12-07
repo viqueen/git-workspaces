b1483ef161b6793b5d991261748f92f2ca61a4d2
Hasnae <hasnae@viqueen.org>
2022-12-07 15:42:38 +1100

    provide different display options

--- src/bin/git-workspace.ts
+++ src/bin/git-workspace.ts
@@ -1,16 +1,52 @@
 #! /usr/bin/env node
 
-import { getConfiguration, itemFilter, withProgram, WithProgram } from '../lib';
+import {
+    getConfiguration,
+    itemFilter,
+    itemLocation,
+    withProgram,
+    WithProgram
+} from '../lib';
 
-const listRepos: WithProgram = ({ registry }, program) => {
-    program.action(async (opts) => {
-        const { workspace, namespace, host, slug, keyword } = opts;
-        await registry
-            .list(itemFilter({ workspace, namespace, host, slug, keyword }))
-            .then((items) =>
-                console.table(items, ['workspace', 'namespace', 'slug'])
-            );
-    });
+const listRepos: WithProgram = ({ workspacesRoot, registry }, program) => {
+    program
+        .option('-dl, --display-location', 'display location only', false)
+        .option('-dc, --display-connection', 'display connection only', false)
+        .option('-ds, --display-slug', 'display slug only', false)
+        .action(async (opts) => {
+            const { workspace, namespace, host, slug, keyword } = opts;
+            await registry
+                .list(itemFilter({ workspace, namespace, host, slug, keyword }))
+                .then((items) => {
+                    const { displayLocation, displayConnection, displaySlug } =
+                        opts;
+                    const displayOptionEnabled =
+                        displayLocation || displayConnection || displaySlug;
+
+                    if (!displayOptionEnabled) {
+                        console.table(items, [
+                            'workspace',
+                            'namespace',
+                            'slug'
+                        ]);
+                        return;
+                    }
+
+                    items.forEach((item) => {
+                        if (displayLocation) {
+                            const target = itemLocation({
+                                workspacesRoot,
+                                item
+                            });
+                            console.info(target);
+                        } else if (displayConnection) {
+                            console.info(item.urlConnection);
+                        } else if (displaySlug) {
+                            console.info(item.slug);
+                        }
+                    });
+                });
+        });
 };
 
 getConfiguration().then(withProgram(listRepos));
