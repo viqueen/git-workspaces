49ceea5ee9bd579b2f7461dbcf0c6c504f3a9ca7
Hasnae <hasnae@viqueen.org>
2023-01-19 10:12:11 +1100

    increase test coverage

---
+++ src/lib/select-and-checkout-branch.test.ts
@@ -0,0 +1,34 @@
+import { selectAndCheckoutBranch } from './select-and-checkout-branch';
+
+jest.mock('inquirer', () => {
+    return {
+        prompt: jest.fn(() => {
+            return Promise.resolve({
+                branchDetails: 'noissue/check-me-out'
+            });
+        })
+    };
+});
+
+jest.mock('simple-git', () => {
+    return () => ({
+        checkout: jest.fn(() => {
+            return Promise.resolve('DONE');
+        })
+    });
+});
+
+describe('select-and-checkout-branch', () => {
+    it('should select and checkout branch', async () => {
+        const result = await selectAndCheckoutBranch(
+            /(?<branchName>noissue\/.*)/
+        )(['does-not-matter']);
+        expect(result).toEqual('DONE');
+    });
+    it('should not checkout the branch if the pattern does not match', async () => {
+        const result = await selectAndCheckoutBranch(
+            /(?<branchName>feature\/.*)/
+        )(['does-not-matter']);
+        expect(result).toBeUndefined();
+    });
+});
