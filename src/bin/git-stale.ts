55d25590bee3b8dfe26c751e0c4305e10cb5caef
Hasnae <hasnae@viqueen.org>
2023-02-23 21:42:49 +1100

    welcome git stale to help clean up local branches

---
+++ src/bin/git-stale.ts
@@ -0,0 +1,15 @@
+import { listRecentBranches } from '../lib/list-recent-branches';
+import { selectAndDeleteBranches } from '../lib/select-and-delete-branches';
+
+const excludeOperationalBranches = async (branches: string[]) => {
+    return branches.filter(
+        (branch) =>
+            !branch.match(
+                /^\d{4}-\d{2}-\d{2} (main|master|production|demo|website|development)$/
+            )
+    );
+};
+
+listRecentBranches()
+    .then(excludeOperationalBranches)
+    .then(selectAndDeleteBranches(/\d{4}-\d{2}-\d{2} (?<branchName>.*)/));
