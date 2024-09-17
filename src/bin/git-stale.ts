9d77630911b8a81f4697d484c0ccd3a419786200
viqueen <hasnae@viqueen.org>
2024-09-17 20:23:40 +1000

    use regexp exec

--- src/bin/git-stale.ts
+++ src/bin/git-stale.ts
@@ -18,12 +18,12 @@
 import { listRecentBranches } from '../lib/list-recent-branches';
 import { selectAndDeleteBranches } from '../lib/select-and-delete-branches';
 
+const mainBranches = /^\d{4}-\d{2}-\d{2} (main|master|production|demo|website|development)$/;
+
 const excludeOperationalBranches = async (branches: string[]) => {
     return branches.filter(
         (branch) =>
-            !branch.match(
-                /^\d{4}-\d{2}-\d{2} (main|master|production|demo|website|development)$/
-            )
+            !mainBranches.exec(branch)
     );
 };
 
